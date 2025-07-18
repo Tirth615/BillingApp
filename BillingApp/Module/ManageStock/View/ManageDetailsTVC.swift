91423b703e61059b8391301d97b4b2795f4f01ff
Tirth615 <shahtirth615@gmail.com>
2025-07-18 12:35:28 +0530

    Manage Details Add

---
+++ BillingApp/Module/ManageStock/View/ManageDetailsTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ManageDetailsTVC.swift
+//  BillingApp
+//
+//  Created by Tirth Shah on 18/07/25.
+//
+
+import UIKit
+
+class ManageDetailsTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
